299652576a120e58c1a86c0c6596fd340a14ec98
Aleksandr Davidenko <alex1190119022@gmail.com>
2019-12-15 19:04:11 +0200

    Добавьте файлы проекта.

---
+++ Queue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            queue.Enqueued("One");
+            queue.Enqueued("Two");
+            queue.Enqueued("Three");
+            queue.Enqueued("Four");
+            queue.Enqueued("Five");
+            queue.Enqueued("Six");
+
+            var item1 = queue.Count;
+            Console.WriteLine($"В очереди {item1} элеиентов.");
+
+            var item2 = queue.Dequeued();
+            Console.WriteLine($"Первый элемент очереди  - {item2} ");
+
+            var item3 = queue.Dequeued();
+            Console.WriteLine($"Второй элемент очереди  - {item3} ");
+
+           
+            Console.WriteLine($"В очереди теперь  {item1} элеиентов.");
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Queue<T>
+    {
+        private List<T> _item = new List<T>();
+
+        public int Count => _item.Count();
+
+        public void Enqueued(T item)
+        {
+            if (item==null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _item.Add(item);
+        }
+       
+        public T Dequeued()
+        {
+            var item = GetItem();
+            _item.Remove(item);
+            return item;
+        }
+        public T Peek()
+        {
+            var item = GetItem();
+
+            return item;
+        }
+        private T GetItem()
+        {
+            var item = _item.FirstOrDefault();
+
+            if (item == null)
+            {
+                throw new NullReferenceException("Очередь пуста");
+            }
+            return item;
+        }
+
+    }
+}
